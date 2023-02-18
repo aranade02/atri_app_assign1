209b4a1bbaacb6d5b9cbd8e01761d5bb5194309f
Utkarsha Ranade <uranade4700@gmail.com>
2023-02-18 16:15:41 +0530

    first commit

---
+++ atri_app/app/src/page-cbs/Home.js
@@ -0,0 +1,46 @@
+import { useCallback } from "react";
+import { callbackFactory } from "../utils/callbackFactory";
+export function useFlex1Cb() {
+	const onClick = useCallback(callbackFactory("Flex1", "Home", "/", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useMenu1Cb() {
+	const onClick = useCallback(callbackFactory("Menu1", "Home", "/", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "controlled",
+      "selector": [
+        "custom",
+        "open"
+      ]
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useTextBox2Cb() {
+	const onClick = useCallback(callbackFactory("TextBox2", "Home", "/", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
---
+++ atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { Flex } from "@atrilabs/react-component-manifests/src/manifests/Flex/Flex.tsx";
+import { Menu } from "@atrilabs/react-component-manifests/src/manifests/Menu/Menu.tsx";
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { useFlex1Cb, useMenu1Cb, useTextBox2Cb } from "../page-cbs/Home";
+import "../page-css/Home.css";
+import "../custom/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [location])
+
+  const Flex1Props = useStore((state)=>state["Home"]["Flex1"]);
+const Flex1IoProps = useIoStore((state)=>state["Home"]["Flex1"]);
+const Flex1Cb = useFlex1Cb()
+const Menu1Props = useStore((state)=>state["Home"]["Menu1"]);
+const Menu1IoProps = useIoStore((state)=>state["Home"]["Menu1"]);
+const Menu1Cb = useMenu1Cb()
+const TextBox2Props = useStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2IoProps = useIoStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2Cb = useTextBox2Cb()
+
+  return (<>
+  <Flex className="p-Home Flex1 bpt" {...Flex1Props} {...Flex1Cb} {...Flex1IoProps}>
+<Menu className="p-Home Menu1 bpt" {...Menu1Props} {...Menu1Cb} {...Menu1IoProps}/>
+<TextBox className="p-Home TextBox2 bpt" {...TextBox2Props} {...TextBox2Cb} {...TextBox2IoProps}/>
+</Flex>
+  </>);
+}
